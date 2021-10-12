c5279ec6c044eb83b6a0854cbda4342da34033f0
任我行 <skywalk94@foxmail.com>
2021-10-12 10:40:29 +0800

    弹窗返回关闭

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import router from './router'
 import 'lib-flexible'
 
 import vuex from './store'
-import { Button } from 'vant';
+import { Button, Popup } from 'vant';
 
 const app = createApp(App)
 app.use(Button)
+app.use(Popup)
 app.use(router)
 app.use(vuex)
 app.mount('#app')
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,27 +1,45 @@
 <template>
-  <button class="btn" @click="goMain()">{{ msg }}</button>
-  <button class="btn" @click="update">{{ state.msg }}</button>
-  <van-button onclick="alert(111111)">点击弹窗</van-button>
-  <van-button type="primary">主要按钮</van-button>
+  <van-button type="success" @click="goMain()">{{ msg }}</van-button>
+  <van-button @click="showPop()">点击弹窗</van-button>
+  <van-popup
+    v-model:show="state.isPop"
+    position="bottom"
+    :style="{ height: '30%' }"
+    @close="addPopState()"
+  />
 </template>
 <script setup>
-import { reactive, onMounted, ref } from "vue"
+import { reactive, onMounted, ref, onBeforeUnmount } from "vue"
 import { useRouter } from "vue-router"
 import { useStore } from "vuex"
 
 const router = useRouter()
 const store = useStore()
+
 let msg = ref("页面跳转")
 const state = reactive({
-  msg: "修改值",
+  isPop: false
 })
 
 onMounted(() => {
   console.log(store.state.num)
+  window.addEventListener("popstate", addPopState, false);
+})
+onBeforeUnmount(() => {
+  window.removeEventListener("popstate", addPopState, false)
 })
 
-const update = () => {
-  state.msg = "修改后的值"
+const showPop = () => {
+  state.isPop = true
+  window.history.pushState(null, null, "#");
+}
+
+const addPopState = () => {
+  if (state.isPop) {
+    state.isPop = false
+  } else {
+    router.go(-1);
+  }
 }
 
 const goMain = () => {
